d9ee607573d58f6f433004266868960c5baea81e
Tushar Pethkar <pethkartusharnarendra@gmail.com>
2022-11-21 23:27:08 +0530

    day1

--- src/Modals/Gift/GiftPopup.js
+++ src/Modals/Gift/GiftPopup.js
@@ -1,7 +1,28 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './style.css';
+import img from '../../images/gifts.png';
 const GiftPopup = () => {
-  return <div>GiftPopup</div>;
+  const [modal, setModal] = useState(true);
+  const toggle = () => {
+    setModal(!modal);
+  };
+  return (
+    <>
+      <button onClick={toggle}>click</button>
+      <div
+        className="container"
+        style={modal ? { display: 'flex' } : { display: 'none' }}
+      >
+        <div className="card">
+          <div className="close" onClick={toggle}></div>
+          <div className="col1">
+            <img src={img} />
+          </div>
+          <div className="col2"></div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default GiftPopup;
